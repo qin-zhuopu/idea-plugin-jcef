66cc4e14f2858b57552f71c27285ad5c74285625
qinshuguo <shuguo.qin@jereh.com>
2024-12-14 14:10:41 +0800

    Executing JavaScript

--- src/main/kotlin/com/github/qinzhuopu/ideapluginjcef/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/qinzhuopu/ideapluginjcef/toolWindow/MyToolWindowFactory.kt
@@ -7,6 +7,10 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
 import com.intellij.ui.jcef.JBCefBrowser
+import org.cef.browser.CefBrowser
+import org.cef.browser.CefFrame
+import org.cef.handler.CefLoadHandler
+import org.cef.network.CefRequest
 import javax.swing.BoxLayout
 
 
@@ -25,6 +29,31 @@ class MyToolWindowFactory : ToolWindowFactory {
 
         val url = "https://example.com/"
         val browser = JBCefBrowser.createBuilder().setUrl(url).setOffScreenRendering(false).build()
+        browser.jbCefClient.addLoadHandler(object : CefLoadHandler {
+            override fun onLoadEnd(cefBrowser: CefBrowser?, p1: CefFrame?, p2: Int) {
+                thisLogger().warn("onLoadEnd")
+                cefBrowser?.executeJavaScript(
+                    """
+                        let now = new Date();
+                        setInterval(() => {
+                          console.log(now);
+                        }, 2000);
+                    """, url, 0
+                )
+            }
+
+            override fun onLoadingStateChange(p0: CefBrowser?, p1: Boolean, p2: Boolean, p3: Boolean) {
+                thisLogger().warn("onLoadingStateChange")
+            }
+
+            override fun onLoadStart(p0: CefBrowser?, p1: CefFrame?, p2: CefRequest.TransitionType?) {
+                thisLogger().warn("onLoadStart")
+            }
+
+            override fun onLoadError(p0: CefBrowser?, p1: CefFrame?, p2: CefLoadHandler.ErrorCode?, p3: String?, p4: String?) {
+                thisLogger().warn("onLoadError")
+            }
+        }, browser.cefBrowser)
         myPanel.add(browser.component)
 
         val content = ContentFactory.getInstance().createContent(myPanel, null, false)
